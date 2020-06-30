f82ed844e68a0ff55eefab7806020a05d5d07d76
mauriciohike <mauriciorenanvs@gmail.com>
2020-06-29 23:02:00 -0300

    init components

---
+++ src/components/Button/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Button = ({
+    color = 'primary',
+    width = 100
+}) => {
+  return <div />;
+}
+
+export default Button;
